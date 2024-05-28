ec0849f2b5472062435bcb60055d5f8942f3651d
Flavia Macovei <diama.m@macovei.eu>
2024-05-28 08:44:28 +0200

    experiments with asynchronisation

--- preprocessing/filter_data_with_ollama.py
+++ preprocessing/filter_data_with_ollama.py
@@ -1,15 +1,17 @@
 from ask_llm import ask_llm
 import datasets
-import ollama
+from ollama import chat
 import numpy as np
+import time
+import asyncio
 
 dataset = datasets.load_dataset("roneneldan/TinyStories", split="train")
 
 # used for testing
-dataset = dataset.select(range(10))
+dataset = dataset.select(range(5))
 
 
-prompt = "Please evaluate the following story based on the following six categories: Grammar, Creativity, Consistency, Originality, Vocabulary, and Overall Enjoyment. Each category should be scored with an integer between 1 and 10, where 1 is the lowest score and 10 is the highest. The output should be in the format of six numbers separated by colons, for example, '5:8:3:9:10:4'. Output the scores only in the following format: <Grammar score>:<Creativity score>:<Consistency score>:<Originality score>:<Vocabulary score>:<Overall Enjoyment score>"
+prompt = "Please evaluate the following story based on the following six categories: Grammar, Creativity, Consistency, Originality, Vocabulary, and Overall Enjoyment. Each category should be scored with an integer between 1 and 10, where 1 is the lowest score and 10 is the highest. The output should be in the format of six numbers separated by colons, for example, '5:8:3:9:10:4'. Output the scores only in the following format: <Grammar score>:<Creativity score>:<Consistency score>:<Originality score>:<Vocabulary score>:<Overall Enjoyment score>. Output nothing but the scores, no additional text or explanation."
 
 
 def parse_ollama_output(output):
@@ -17,20 +19,79 @@ def parse_ollama_output(output):
     scores = [int(score) for score in score_strings]
     return scores
 
-def ask_llm(system_prompt, datapoint):
+async def ask_llm(system_prompt, datapoint):
     msgs = [
         {"role": "system", "content": system_prompt},
         { "role": "user", "content": datapoint },
     ]
-    output = ollama.chat(model="llama3", messages=msgs )
-
+    start_time = time.time()
+    output = chat(model="llama3", messages=msgs)
+    end_time = time.time()
+    print(f"Elapsed time: {end_time - start_time}")
     return(output['message']['content'])
 
-for i in range(0, len(dataset['text'])):
-    ollama_output = ask_llm(prompt, dataset['text'][i])
+async def score_story(story):
+    ollama_output = await ask_llm(prompt, story)
     scores = parse_ollama_output(ollama_output)
-    print(scores)
+    print(np.average(scores))
+    return np.average(scores)
 
+# for i in range(0, len(dataset['text'])):
+#     ollama_output = ask_llm(prompt, dataset['text'][i])
+#     scores = parse_ollama_output(ollama_output)
+#     print(scores)
+
+
+def ask_llm(system_prompt, datapoints):
+    msgs = [{"role": "system", "content": system_prompt}]
+
+    for datapoint in datapoints:
+        msgs.append(
+        { "role": "user", "content": datapoint }
+        )
+    start_time = time.time()
+    output = chat(model="llama3", messages=msgs)
+    end_time = time.time()
+    print(f"Elapsed time: {end_time - start_time}")
+    return(output['message']['content'])
+
+result = ask_llm(prompt, dataset['text'])
+
+print(result)
+
+def score_story(story):
+    ollama_output = ask_llm(prompt, story)
+    scores = parse_ollama_output(ollama_output)
+    print(np.average(scores))
+    return np.average(scores)
+
+
+
+
+
+
+
+for story in dataset['text']:
+    asyncio.run(score_story(story))
+
+
+
+
+min_score = 3
+
+_start_time = time.time()
+
+# mylist_annotated = [await score_story(x['text']) for x in dataset]
+for story in dataset:
+    print(await score_story(story['text']))
+
+# hq_stories = dataset.filter(lambda story: run(score_story(story['text'])) > min_score)
+
+_end_time = time.time()
+
+print(f"Elapsed time: {_end_time - _start_time}")
+
+print(mylist_annotated)
 
 # for later:
 # min_score = 3
--- preprocessing/remove_forbidden_letters.py
+++ preprocessing/remove_forbidden_letters.py
@@ -1,44 +1,19 @@
-# from used_letters import forbidden_letters
-# from used_letters import process_string
+from preprocessing.used_letters import forbidden_letters
 import datasets
-import re
 import time
 
-forbidden_letters = {'ê', '_', 'ò', '²', 'š', 'ž', 'ª', 'á', 'ÿ', 'ï', '½', 'ä', 'æ', 'ˆ', 'ã', 'ñ', 'º', '³', '¾', '¼', 'é', 'â', 'œ', 'µ', 'î', 'è', 'å', 'ç', '¹', 'ð'}
-
-def process_string(text):
-    text = text.lower()
-    text = re.sub(r"n't", " not", text)
-    text = re.sub(r"'(m|re|s|d|ll|ve)", r" '\1", text)
-
-    # Remove punctuation marks except apostrophes
-    text = re.sub(r'[^\w\s\']', '', text)
-
-    return text
-
-
 dataset = datasets.load_dataset("roneneldan/TinyStories", split="train")
-# dataset = dataset.select(range(10000))
 
-# list all indices of stories which contain a forbidden letter
-forbidden_indices = []
+
 
 start_time = time.time()
 
-for i in range(0, len(dataset['text'])):
-    if i == 100:
-        end_time = time.time()
-    processed_story = process_string(dataset['text'][i])
-    if any(letter in processed_story for letter in forbidden_letters):
-        forbidden_indices.append(i)
+filtered_dataset = dataset.filter(lambda story: not any(char in story['text'].lower() for char in forbidden_letters))
+
+end_time = time.time()
 
 print(f"Elapsed time: {end_time - start_time}s")
 
-print(len(forbidden_indices))
-print(forbidden_indices)
-
-forbidden_indices_as_string = str(forbidden_indices)
-
-f = open("forbidden_indices.txt", "w")
-f.write(forbidden_indices_as_string)
-f.close()
+print(len(filtered_dataset))
+print(filtered_dataset)
+print(len(dataset['text']))
